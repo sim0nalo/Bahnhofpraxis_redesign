3bc3f506c9cdb1bf89a096e2fb72ece70a1ad0fe
Simona <127302184+sim0nalo@users.noreply.github.com>
2024-11-05 14:11:02 +0000

    alles mögliche

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 import { engine } from "express-handlebars";
 import pg from "pg";
 const { Pool } = pg;
@@ -30,6 +31,46 @@ export function createApp(dbconfig) {
   );
 
   app.locals.pool = pool;
+
+  app.get("/register", function (req, res) {
+    res.render("register");
+  });
+
+  app.post("/register", function (req, res) {
+    var passwort = bcrypt.hashSync(req.body.passwort, 10);
+    pool.query(
+      "INSERT INTO users (name, passwort) VALUES ($1, $2)",
+      [req.body.name, passwort],
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        }
+        res.redirect("/login");
+      }
+    );
+  });
+
+  app.get("/login", function (req, res) {
+    res.render("login");
+  });
+
+  app.post("/login", function (req, res) {
+    pool.query(
+      "SELECT * FROM users WHERE name = $1",
+      [req.body.name],
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        }
+        if (bcrypt.compareSync(req.body.passwort, result.rows[0].passwort)) {
+          req.session.userid = result.rows[0].id;
+          res.redirect("/");
+        } else {
+          res.redirect("/login");
+        }
+      }
+    );
+  });
 
   return app;
 }
--- index.js
+++ index.js
@@ -23,6 +23,14 @@ app.get("/impressum", async function (req, res) {
 });
 
 app.get("/profil", async function (req, res) {
+  const posts = await app.locals.pool.query(
+    "SELECT * FROM posts WHERE user_id = $1",
+    [req.session.userid]
+  );
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
   res.render("profil", {});
 });
 
@@ -38,6 +46,10 @@ app.get("/logout", async function (req, res) {
 });
 
 app.get("/new_post", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
   res.render("new_post", {});
 });
 
@@ -45,22 +57,19 @@ app.get("/login", async function (req, res) {
   res.render("login", {});
 });
 
-/* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
-app.listen(3010, () => {
-  console.log(`Example app listening at http://localhost:3010`);
-});
-
 /*Formular_Start*/
-app.get("/new_post", async function (req, res) {
-  res.render("new_post", {});
-});
 
 app.post("create_post", async function (req, res) {
   await app.locals.pool.query(
-    "INSERT INTO posts (cerated_at, title, text) VALUES ($1, $2, $3)",
-    [req.body.created_at, req.body.title, req.body.text]
+    "INSERT INTO posts (created_at, title, text, user_id) VALUES ($1, $2, $3, $4)",
+    [req.body.created_at, req.body.title, req.body.text, req.session.userid]
   );
   res.redirect("/");
 });
 
 /*Formular_Ende*/
+
+/* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
+app.listen(3010, () => {
+  console.log(`Example app listening at http://localhost:3010`);
+});
